971764d8c2a229660a9553aa364c782eeac84caa
drpulido <drpulido@bancolombia.com.co>
2025-06-09 17:43:57 -0500

    adicion ejemplo reemplazo cadenas

---
+++ src/ReemplazarSubcadenas.java
@@ -0,0 +1,11 @@
+public class ReemplazarSubcadenas {
+    public static void main(String[] args) {
+        //Reemplazar cadenas
+        var cadena1 = "Hola Mundo";
+        System.out.println("cadena1 = " + cadena1);
+
+        var cadena2 = cadena1.replace("Mundo", "a todos");
+        System.out.println("cadena2 = " + cadena2);
+
+    }
+}
